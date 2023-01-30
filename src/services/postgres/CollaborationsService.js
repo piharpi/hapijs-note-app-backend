a5ce27c137f0dc909259872c33d2d3aa03b500cd
Mahendrata Harpi <justharpi@gmail.com>
2023-01-30 17:46:45 +0700

    feat: implemented collaborations functional

---
+++ src/services/postgres/CollaborationsService.js
@@ -0,0 +1,55 @@
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+
+const InvariantError = require('../../exceptions/InvariantError');
+
+class CollaborationsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addCollaboration(noteId, userId) {
+    const id = `collab-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO collaborations VALUES($1, $2, $3) RETURNING id',
+      values: [id, noteId, userId],
+    };
+
+    const { rows, rowCount } = await this._pool.query(query);
+
+    if (!rowCount) {
+      throw new InvariantError('Kolaborasi gagal ditambahkan');
+    }
+
+    return rows[0].id;
+  }
+
+  async deleteCollaboration(noteId, userId) {
+    const query = {
+      text: 'DELETE FROM collaborations WHERE note_id = $1 AND user_id = $2 RETURNING id',
+      values: [noteId, userId],
+    };
+
+    const { rowCount } = await this._pool.query(query);
+
+    if (!rowCount) {
+      throw new InvariantError('Kolaborasi gagal dihapus');
+    }
+  }
+
+  async verifyCollaborator(noteId, userId) {
+    const query = {
+      text: 'SELECT * FROM collaborations WHERE note_id = $1 AND user_id = $2',
+      values: [noteId, userId],
+    };
+
+    const { rowCount } = await this._pool.query(query);
+
+    if (!rowCount) {
+      throw new InvariantError('Kolaborasi gagal diverifikasi');
+    }
+  }
+}
+
+module.exports = CollaborationsService;
