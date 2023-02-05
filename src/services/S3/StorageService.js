29213d3df46b74fbcfa7ba9fd61d9b528932258e
Mahendrata Harpi <justharpi@gmail.com>
2023-02-05 10:50:40 +0700

    feat!: S3 storage

---
+++ src/services/S3/StorageService.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+
+class StorageService {
+  constructor() {
+    this._S3 = new AWS.S3();
+  }
+
+  writeFile(file, meta) {
+    const parameter = {
+      Bucket: process.env.AWS_BUCKET_NAME, // Bucket Name
+      Key: +new Date() + meta.filename, // Filename
+      Body: file._data, // Buffer
+      ContentType: meta.headers['content-type'], // MIME Type
+    };
+
+    return new Promise((resolve, reject) => {
+      this._S3.upload(parameter, (error, data) => {
+        if (error) {
+          return reject(error);
+        }
+
+        return resolve(data.Location);
+      });
+    });
+  }
+}
+
+module.exports = StorageService;
